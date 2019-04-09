a6fff9e0333655761e062529771dd31a8b4d4dde
Parker Lusk <parkerclusk@gmail.com>
2019-04-09 11:20:39 -0400

    init. rollaball example finished

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = player.transform.position + offset;
+	}
+
+    // Runs every frame, but guaranteed to run after all items have
+    // been processed and updated. So when we set position of the camera,
+    // we know absolutely that the player has been updated for this frame.
+    // Best for "follow cameras, procedural animations, and gathering
+    // last known states."
+    void LateUpdate () {
+
+    }
+}
